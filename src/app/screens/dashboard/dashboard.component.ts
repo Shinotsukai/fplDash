796067a19f79951d9f822128278c21628a72a9ef
Jon Savage <jon@block47.co.uk>
2021-10-23 14:53:31 +0100

    dashboard call api

--- src/app/screens/dashboard/dashboard.component.ts
+++ src/app/screens/dashboard/dashboard.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BehaviorSubject } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard-new.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public apiResponse: BehaviorSubject<any> = new BehaviorSubject({})
+  constructor(private apiService: ApiService) { }
+  async ngOnInit(): Promise<void> {
+    this.apiResponse.next(await this.apiService.getTeamById(166603).toPromise()); // TODO: Move Id to somewhere much better
+    console.log(this.apiResponse.getValue())
   }
-
 }
