1f0a0bc993861fd6e07741c32e1587119ce326a3
Jon Savage <jon@block47.co.uk>
2021-10-23 14:53:42 +0100

    add pitch-player to dashboard

--- src/app/screens/dashboard/dashboard.module.ts
+++ src/app/screens/dashboard/dashboard.module.ts
@@ -2,17 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { RouterModule } from '@angular/router';
-
-
-
+import { PitchPlayerModule } from 'src/app/components/pitch-player/pitch-player.module';
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(
-     [ {path:'',pathMatch:'full',component:DashboardComponent}]
-    )
+      [{ path: '', pathMatch: 'full', component: DashboardComponent }]
+    ),
+    PitchPlayerModule
   ],
-  exports:[DashboardComponent]
+  exports: [DashboardComponent]
 })
 export class DashboardModule { }
