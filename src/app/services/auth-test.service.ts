05b4de8bad88f41416587b1f3bdc9179d25880ef
Jon Savage <jon@block47.co.uk>
2021-10-23 14:52:31 +0100

    tidy authTest service

--- src/app/services/auth-test.service.ts
+++ src/app/services/auth-test.service.ts
@@ -1,22 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, from, Observable } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
-
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 const TOKEN_KEY = 'auth-token';
-
 @Injectable({
   providedIn: 'root'
 })
 export class AuthTestService {
-
   isAuthenticated: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   token = '';
-
   constructor(private http: HttpClient) {
     this.getToken();
   }
-
   async getToken() {
     const token = await localStorage.getItem(TOKEN_KEY);
     if (token) {
@@ -26,19 +21,13 @@ export class AuthTestService {
       this.isAuthenticated.next(false);
     }
   }
-
-  login(credentials: { email:any; password:any }): Observable<any> {
+  login(credentials: { email: any; password: any }): Observable<any> {
     return this.http.post(`https://reqres.in/api/login`, credentials).pipe(
-    map((data:any)=>{
-      return localStorage.setItem(TOKEN_KEY,data.token);
-    }),
-      tap((_) => {
-        this.isAuthenticated.next(true);
-      })
+      map((data: any) => { localStorage.setItem(TOKEN_KEY, data.token)}),
+      tap(() => { this.isAuthenticated.next(true)})
     );
   }
-
-  logout():Promise<void> {
+  logout(): Promise<void> {
     this.isAuthenticated.next(false);
     return localStorage.remove({ key: TOKEN_KEY });
   }
